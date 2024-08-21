6d8c6a4627c0b49b9c2e1517273513cdef0034bc
ardhikaxx <ardhikayanuar58@gmail.com>
2024-08-21 16:19:08 +0700

    update + add

--- js/script.js
+++ js/script.js
@@ -22,22 +22,76 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 });
 
+document.getElementById('whatsappForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const nama = document.getElementById('nama').value;
+    const telepon = document.getElementById('telepon').value;
+    const pesan = document.getElementById('pesan').value;
+    const whatsappLink = `https://wa.me/${telepon}`;
+    const message = `Assalamu'alaikum, nama saya ${nama}. Saya ingin menyampaikan: ${pesan}. Anda dapat menghubungi saya melalui WhatsApp di: ${whatsappLink}. Terima kasih.`;
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappUrl = `https://api.whatsapp.com/send?phone=6281334543419&text=${encodedMessage}`;
+    window.open(whatsappUrl, '_blank');
+});
+
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll(".nav-link");
+    ScrollReveal().reveal('.img-fluid', {
+        origin: 'top',
+        distance: '100px',
+        duration: 1200,
+        easing: 'ease-in-out',
+        delay: 200
+    });
 
-    links.forEach(link => {
-        link.addEventListener("click", function (e) {
-            e.preventDefault();
+    ScrollReveal().reveal('.hero-section h1', {
+        origin: 'top',
+        distance: '100px',
+        duration: 1200,
+        easing: 'ease-in-out',
+        delay: 200
+    });
 
-            const targetId = this.getAttribute("href").substring(1);
-            const targetElement = document.getElementById(targetId);
+    ScrollReveal().reveal('.hero-section p', {
+        origin: 'top',
+        distance: '100px',
+        duration: 1200,
+        easing: 'ease-in-out',
+        delay: 300
+    });
 
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop - 70,
-                    behavior: "smooth"
-                });
-            }
-        });
+    ScrollReveal().reveal('.hero-section .btn', {
+        origin: 'bottom',
+        distance: '50px',
+        duration: 1200,
+        easing: 'ease-in-out',
+        delay: 400
+    });
+
+    ScrollReveal().reveal('#layanan', {
+        interval: 200,
+        origin: 'bottom',
+        distance: '50px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        delay: 150
+    });
+
+    ScrollReveal().reveal('#syarat', {
+        interval: 200,
+        origin: 'bottom',
+        distance: '50px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        delay: 150
+    });
+
+    ScrollReveal().reveal('#kontak', {
+        origin: 'bottom',
+        distance: '50px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        delay: 150
     });
 });
+
+document.getElementById("currentYear").textContent = new Date().getFullYear();
